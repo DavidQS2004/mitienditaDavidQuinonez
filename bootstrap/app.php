a1c3475896afbaa6baf15480853379e2590e3e1d
David Quinonez <davidesnaq@gmail.com>
2026-06-05 14:36:06 -0500

    correccion de errores

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\URL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,15 +10,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })
-    ->booted(function () {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
-    })
-    ->create();
+    })->create();

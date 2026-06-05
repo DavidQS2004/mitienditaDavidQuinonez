878b687a4b401aa54c2e7d84cf734e5aadf76212
David Quinonez <davidesnaq@gmail.com>
2026-06-05 15:23:52 -0500

    provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,22 +6,21 @@ use Illuminate\Support\ServiceProvider;
 use App\Models\Order;
 use App\Policies\OrderPolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-            Gate::policy(Order::class, OrderPolicy::class);
+        Gate::policy(Order::class, OrderPolicy::class);
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
